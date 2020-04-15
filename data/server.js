14c9c1463fe5bd50e6250d9d627cda8b242ba99a
ptol <naughton.joshua@gmail.com>
2020-04-15 18:44:19 -0400

    Client sends URL array, server wgets

--- data/server.js
+++ data/server.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
+const request = require('request');
 
-const zipfile = '/cover-data/testimages.zip';
+const datadir = `/cover-data`;
+const zipfile = `${datadir}/testimages.zip`;
 
-function checkFileExistsSync(filepath){
+function checkFileExistsSync(path){
   let flag = true;
   try{
-    fs.accessSync(filepath, fs.F_OK);
+    fs.accessSync(path, fs.F_OK);
   }catch(e){
     flag = false;
   }
@@ -21,6 +23,36 @@ function getImageArchive(path) {
   }
 }
 
+function isValidJSON(str) {
+  try { JSON.parse(str); }
+  catch (e) { return false; }
+  return true;
+}
+
+function isValidImageURL(image_url) {
+  return true;
+}
+
+function wgetImageURL(image_url) {
+  let filename = image_url.toString().split('/').pop();
+  if (checkFileExistsSync(`${datadir}/${filename}`)) {
+    console.log (`WARN: wgetImageURL skipped for ${filename}, already exists.`);
+  } else {
+    try { request(image_url).pipe(fs.createWriteStream(`${datadir}/${filename}`)); }
+    catch (e) { console.log(`ERROR: wgetImageURL failure: ${e}`); }
+  }
+}
+
+function processImageArray(imagelist) {
+  imagelist.forEach( image_url => {
+    console.log("Processing URL: " + image_url);
+    if (isValidImageURL(image_url)) {
+      wgetImageURL(image_url);
+    } else {
+      console.log(`ERROR: failed isValidImageURL ${image_url}`);
+    }
+  });
+}
 
 console.log("Starting Node WebSocket server on 8011...");
 
@@ -35,6 +67,13 @@ var WebSocketServer = require('ws').Server
           ws.binaryType = "blob";
           console.log(`Pack is packed, type set to: ${ws.binaryType}.`);
           ws.send(pack);
+        } else if (isValidJSON(message)) {
+          ws.send("Server received valid JSON from client.");
+          let imagelist = JSON.parse(message);
+          if (Array.isArray(imagelist)) {
+            console.log("Passed the isArray test, attempting processing");
+            processImageArray(imagelist);
+          }
         } else {
           console.log('Received from client: %s', message);
           ws.send('Server received from client: ' + message);
