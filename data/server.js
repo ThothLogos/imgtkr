5982f457436e6b469d9e6b8828f68caf53f747f3
ptol <naughton.joshua@gmail.com>
2020-04-15 14:19:15 -0400

    Client sending binary Blob

--- data/server.js
+++ data/server.js
@@ -1,4 +1,29 @@
-console.log("Server started");
+const fs = require('fs');
+
+const zipfile = '/cover-data/testimages.zip';
+
+function checkFileExistsSync(filepath){
+  let flag = true;
+  try{
+    fs.accessSync(filepath, fs.F_OK);
+  }catch(e){
+    flag = false;
+  }
+  return flag;
+}
+
+function getImageArchive(path) {
+  if (checkFileExistsSync(path)) {
+    console.log(`Found archive at ${path}, attempting read...`);
+    return fs.readFileSync(path);
+  } else {
+    console.log(`ERROR - could not locate ${path}`);
+  }
+}
+
+
+console.log("Starting Node WebSocket server on 8011...");
+
 var Msg = '';
 var WebSocketServer = require('ws').Server
     , wss = new WebSocketServer({port: 8011});
@@ -6,8 +31,9 @@ var WebSocketServer = require('ws').Server
         ws.on('message', function(message) {
         if (message == "dothething") {
           console.log(' [!] Orders received, attempting send of zip');
+          let pack = getImageArchive(zipfile);
           ws.binaryType = "blob";
-          ws.send('/cover-test/testimages.zip');
+          ws.send(pack);
         } else {
           console.log('Received from client: %s', message);
         }
