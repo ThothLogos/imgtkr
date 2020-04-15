5a39b454e9de0996af9c44e86b8f81ad9cef76fb
ptol <naughton.joshua@gmail.com>
2020-04-14 22:06:52 -0400

    Parse message for behavior

--- data/server.js
+++ data/server.js
@@ -4,7 +4,11 @@ var WebSocketServer = require('ws').Server
     , wss = new WebSocketServer({port: 8011});
     wss.on('connection', function(ws) {
         ws.on('message', function(message) {
-        console.log('Received from client: %s', message);
+        if (message == "dothething") {
+          console.log(' [!] Orders received.');
+        } else {
+          console.log('Received from client: %s', message);
+        }
         ws.send('Server received from client: ' + message);
     });
  });
