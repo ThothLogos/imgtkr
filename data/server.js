be5ce9106480df1be97bb2b35fab7b9aa06e139e
ptol <naughton.joshua@gmail.com>
2020-04-16 02:21:00 -0400

    Switched to system calls for wget and zip

--- data/server.js
+++ data/server.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-const request = require('request');
-const JSZip = require('jszip');
+const child_process = require("child_process");
+//const request = require('request');
+//const JSZip = require('jszip');
+//const FileSaver = require('file-saver');
 
 const datadir = `/cover-data`;
 const zipfile = `${datadir}/testimages.zip`;
@@ -35,11 +37,15 @@ function isValidImageURL(image_url) {
 
 function wgetImageURL(image_url, dir) {
   let img = image_url.toString().split('/').pop();
-  if (fs.existsSync(`${datadir}/${dir}/${img}`)) { // Prevent duplicates
+  if (fs.existsSync(`${dir}/${img}`)) { // Prevent duplicates
     console.log (`WARN: wgetImageURL skipped for ${img}, already exists.`);
   } else {
-    try { request(image_url).pipe(fs.createWriteStream(`${datadir}/${dir}/${img}`)); }
-    catch (e) { console.log(`ERROR wgetImageURL fail: ${e}`); }
+    try {
+      child_process.execSync(`wget ${image_url} -P ${dir}/`);
+      console.log(`File complete: ${img}`);
+//    request(image_url).pipe(fs.createWriteStream(`${dir}/${img}`))
+//    .on('close', () =>  console.log(`File complete: ${img}`));
+    } catch (e) { console.log(`ERROR wgetImageURL fail: ${e}`); }
   }
 }
 
@@ -50,15 +56,15 @@ function createTempImageDir() {
     catch (e) { console.log(`ERROR createTempImageDir fail: ${e}`); }
     console.log(`Created directory: ${datadir}/${now}`);
   }
-  return now;
+  return `${datadir}/${now}`;
 }
 
-function processImageArray(imagelist) {
-  let tempdir = createTempImageDir();
+function processImageArray(imagelist, tempdir) {
   imagelist.forEach( image_url => {
     console.log("Processing URL: " + image_url);
     if (isValidImageURL(image_url)) {
-      wgetImageURL(image_url, tempdir);
+      let wutsdis = wgetImageURL(image_url, tempdir);
+      console.log(`Wut it is: ${typeof wutsdis}`);
     } else {
       console.log(`ERROR isValidImageURL fail: ${image_url}`);
     }
@@ -66,7 +72,38 @@ function processImageArray(imagelist) {
 }
 
 function buildArchiveFromDir(image_dir) {
-  // TODO: Use JSZip to create zipfile from tempdir
+  //let archive = new JSZip();
+  //archive.file(`${image_dir}/${img}`);
+  console.log(`tempdir holding: ${image_dir}`)
+  if (fs.existsSync(`${datadir}/latest.zip`)) { fs.unlinkSync(`${datadir}/latest.zip`) }
+  child_process.execSync(`zip -urj ${datadir}/latest.zip ${image_dir}/*`);
+  if (fs.existsSync(`${datadir}/latest.zip`)) {
+    console.log("latest.zip created");
+  }
+  /*
+  fs.readdirSync(image_dir).forEach( img => {
+    console.log(`Adding ${image_dir}/${img} to zip`);
+    child_process.execSync(`zip -urj ${datadir}/latest.zip ${image_dir}/${img}`);
+  });
+  */
+
+  //let buffer = archive.generateNodeStream({ type:"nodebuffer", streamFiles:true });
+  //fs.writeFileSync(`${datadir}/latest.zip`, buffer);
+  
+  /*
+  archive.generateNodeStream({ type:"nodebuffer", streamFiles:true })
+    .pipe(fs.createWriteStream(`${datadir}/latest.zip`))
+    .on('finish', () => { console.log("Latest.zip written to file.")
+  });
+  */
+
+  /*
+  archive.generateAsync({type : "blob"}).then( file => {
+    let timestampdir = image_dir.split('/').pop();
+    console.log("Attempting zip write");
+    FileSaver.saveAs(file, 'latest.zip')
+  });
+  */
 }
 
 function cleanupTempDir(tempdir) {
@@ -81,17 +118,20 @@ var WebSocketServer = require('ws').Server
     wss.on('connection', function(ws) {
         ws.on('message', function(message) {
         if (message == "dothething") {
+          /*
           console.log(' [!] Orders received, attempting send of zip');
           let pack = getImageArchive(zipfile);
           ws.binaryType = "blob";
           console.log(`Pack is packed, type set to: ${ws.binaryType}.`);
-          ws.send(pack);
+          ws.send(pack); */
         } else if (isValidJSON(message)) {
           ws.send("Server received valid JSON from client.");
           let imagelist = JSON.parse(message);
           if (Array.isArray(imagelist)) {
             console.log("Passed the isArray test, attempting processing");
-            processImageArray(imagelist);
+            let tempdir = createTempImageDir();
+            processImageArray(imagelist, tempdir);
+            buildArchiveFromDir(tempdir);
           }
         } else {
           console.log('Received from client: %s', message);
