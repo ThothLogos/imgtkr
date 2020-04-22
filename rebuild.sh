4f259243546ae3d220d2defbb2cad1e24850ee7a
ptol <naughton.joshua@gmail.com>
2020-04-22 14:50:10 -0400

    Strip out nginx image mods, deprecated

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if ! [[ $(docker images | grep alpinexws:0.1) ]];then
-  docker build -f dockerfiles/nginx/Dockerfile -t thothlogos/alpinexws:0.1 .
-fi
-
 docker-compose down && docker rmi thothlogos/nodews:0.1
-docker build -f dockerfiles/nodews/Dockerfile -t thothlogos/nodews:0.1 . && docker-compose up
+docker build -f ./Dockerfile -t thothlogos/nodews:0.1 . && docker-compose up
 
